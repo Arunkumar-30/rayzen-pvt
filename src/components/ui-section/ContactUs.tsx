d106e64fb43f5c049f80949427c68b467fd0bfd3
Arunkumar-30 <Selvam.arun100@gmail.com>
2026-01-20 12:56:24 +0530

    map

--- src/components/ui-section/ContactUs.tsx
+++ src/components/ui-section/ContactUs.tsx
@@ -206,6 +206,20 @@ export default function ContactSection() {
           </form>
         </div>
       </section>
+        <section>
+        <div data-aos="fade-up" data-aos-duration="2000">
+         
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3917.6255851207297!2d76.98786107586972!3d10.916033356601382!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba85ba3a8e1ff51%3A0x6d9c4a38882fae17!2sRayzen%20Power%20Private%20Limited!5e0!3m2!1sen!2sin!4v1768893861600!5m2!1sen!2sin"
+            width="100%"
+            height="350"
+            style={{ border: 0 }}
+            className="card"
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </section>
     </div>
   );
 }
